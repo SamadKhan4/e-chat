eec7c9321a9c9f9a347d39e2865f7af4855b6b56
Shishirthakur2416 <samadkhan040204@gmail.com>
2026-02-02 15:12:24 +0530

    check

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie']
 }));
 app.use(express.json());
 
